d48e79a5a7ab84bb23f3a18bc1c175570af7af13
Facundo Cruz <facundocruz0604@gmail.com>
2023-06-15 20:36:52 -0300

    Imagen terminado

--- src/main/java/com/zonaop/alquileres/repositorios/ImagenRepositorio.java
+++ src/main/java/com/zonaop/alquileres/repositorios/ImagenRepositorio.java
@@ -1,14 +1,11 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package com.zonaop.alquileres.repositorios;
 
-/**
- *
- * @author Facundo
- */
-public class ImagenRepositorio {
+import com.zonaop.alquileres.entidades.Imagen;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ImagenRepositorio extends JpaRepository<Imagen, String> {
     
 }

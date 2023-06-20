d4fe7e1163e229e0a7f167d46af3e3994db23051
JoaquinG5 <joaquin.ghiorzo@gmail.com>
2023-06-20 15:47:30 -0300

    entidades y sercios reserva,opinion,servicio y sus repositorios

---
+++ src/main/java/com/zonaop/alquileres/repositorios/ServicioRepositorio.java
@@ -0,0 +1,15 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.zonaop.alquileres.repositorios;
+
+import com.zonaop.alquileres.entidades.Servicio;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ServicioRepositorio extends JpaRepository<Servicio, String> {
+    
+}

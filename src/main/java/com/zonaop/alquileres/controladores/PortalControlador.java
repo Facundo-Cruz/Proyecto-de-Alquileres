125446fabf2793e571762bb8f18597e191ee3e05
Facundo Cruz <facundocruz0604@gmail.com>
2023-06-21 18:05:53 -0300

    Nuevos cambios de propiedad

--- src/main/java/com/zonaop/alquileres/controladores/PortalControlador.java
+++ src/main/java/com/zonaop/alquileres/controladores/PortalControlador.java
@@ -1,20 +1,25 @@
-
-
 package com.zonaop.alquileres.controladores;
 
 // @author lauty
-
+import com.zonaop.alquileres.entidades.Propiedad;
+import com.zonaop.alquileres.servicios.PropiedadServicio;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
- @Controller
- @RequestMapping("/")
+@Controller
+@RequestMapping("/")
 public class PortalControlador {
-     
-     @GetMapping("/")
-     public String index() {
-     
-         return "mainPage.html";
- } 
+
+    @Autowired
+    private PropiedadServicio propiedadServicio;
+    @GetMapping("/")
+    public String index(ModelMap model) {
+        List<Propiedad> propiedades = propiedadServicio.listarPropiedades();
+        model.put("propiedades", propiedades);
+        return "mainPage.html";
+    }
 }
